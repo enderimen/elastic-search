a0f3ffeebf5b45d797b4eef8c41aead8ab8564c4
Ender İMEN <enderimen@hotmail.com>
2018-11-29 14:36:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ var elasticsearch = require('elasticsearch');
 // elasticsearh connect
 var client = new elasticsearch.Client( {
   hosts: [
-    'http://elastic-test.emlakjet.com:9200/'
     'localhost/'
   ]
 });
